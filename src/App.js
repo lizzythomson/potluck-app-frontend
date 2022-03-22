3f91c39ce99186351804e2aa3b87382e47147387
Lizzy Thomson <71537606+lizzythomson@users.noreply.github.com>
2022-03-22 13:57:23 -0600

    created basic layout of home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='bg-slate-50 flex-column'>
+      <header className='flex gap-5 w-5/6 flex-row bg-cyan-700 h-24 items-center text-slate-50'>
+        <h2 className='px-4 text-2xl justify-self-end'>Potluck App</h2>
+        <nav className='px-4'>
+          <Link to='/'>Home</Link>
+          <Link to='/register'>Register</Link>
+          <Link to='/login'>Login</Link>
+        </nav>
       </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='register' element={<div>Register</div>} />
+        <Route path='login' element={<div>Login</div>} />
+      </Routes>
+      <footer className='bg-cyan-700 w-5/6 h-24 flex'>
+        <button className='bg-slate-50 rounded-lg w-28 self-center'>
+          Get Started!
+        </button>
+      </footer>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+const Home = () => {
+  return (
+    <div className='w-5/6'>
+      <img
+        src='https://images.unsplash.com/photo-1530062845289-9109b2c9c868?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2972&q=80'
+        alt='gathering with food'
+        className='object-cover h-96 w-full'
+      ></img>
+      <div className='flex-column md:container shadow border-black'>
+        <p className='p-4'>
+          Brownie lemon drops bear claw dragée powder gingerbread cake jelly
+          beans cotton candy. Dessert cake sesame snaps bonbon danish sesame
+          snaps shortbread marshmallow. Bonbon gummies tiramisu caramels
+          gingerbread apple pie cake. Cookie marzipan cookie gummies apple pie.
+        </p>
+        <p className='p-4'>
+          Tootsie roll liquorice chocolate bar tart donut sweet roll tiramisu.
+          Pudding shortbread bonbon gummies powder liquorice marshmallow. Toffee
+          donut jelly dragée gummi bears candy canes sugar plum. Candy muffin
+          danish sugar plum gingerbread marshmallow soufflé chocolate.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
